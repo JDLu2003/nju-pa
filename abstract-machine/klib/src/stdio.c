4a80cd079541fa4d0d62ecff58cfad8f557d21a7
JDLu2003 <1277319667@qq.com.com>
2026-02-09 10:36:52 +0800

    feat: 通过 am 下 cpu-test 全部测试，stdio.c 没有全部完成

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,51 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char *itoa(long long val, char *str, int base) {
+  if (base < 2 || base > 36) {
+    *str = '\0';
+    return str;
+
+  }
+  char *ptr = str;
+  char *ptr1 = str;
+  char tmp_char;
+  long long  tmp_val;
+  bool is_neg = false;
+  
+  if (val < 0 && base == 10) {
+    is_neg = true;
+    tmp_val = -val;
+  } else {
+    tmp_val = val;
+  }
+
+  do {
+    long long  remainder = tmp_val % base;
+    *ptr = (remainder < 10) ? (remainder + '0') : (remainder - 10 + 'a');
+    ptr++;
+    tmp_val /= base;
+  } while (tmp_val > 0);
+
+  if (is_neg) {
+    *ptr = '-';
+    ptr++;
+  }
+
+  *ptr = '\0';
+  ptr--;
+
+  // reverse
+  while(ptr1 < ptr) {
+    tmp_char = *ptr;
+    *ptr = *ptr1;
+    *ptr1 = tmp_char;
+    ptr--;
+    ptr1++;
+  }
+  return str;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,15 +59,69 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  return vsnprintf(out, 65534, fmt, ap);
   panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  return vsnprintf(out, n, fmt, ap);
+  // panic("Not implemented");
 }
 
+// 核心函数，将全部调用这个函数来实现其他功能
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t count = 0;
+  const char *p = fmt;
+  
+  #define EMIT(c) do { \
+    if (count < n - 1) { out[count] = c;} \
+    count++; \
+  } while(0) 
+
+  while(*p) {
+    if (*p != '%') {
+      EMIT(*p);
+      p++;
+    } else {
+      p++;
+      switch (*p)
+      {
+        case 'd':
+        {
+          int val = va_arg(ap, int);
+          char buf[64];
+          char *ptr = itoa(val, buf, 10);
+          while (*ptr) {
+            EMIT(*ptr++);
+          }
+          break;
+        }
+        case 's':
+        {
+          char *val = va_arg(ap, char *);
+          while (*val)
+          {
+            EMIT(*val++);
+          }
+          break;
+        }
+        default:
+          panic("Unsupport type in vsnprint");
+      }
+      p++;
+    }
+  }
+  if (n > 0) {
+    if (count < n) out[count] = '\0';
+      else out[n - 1] = '\0';
+  }
+  return count;
+
+  // panic("Not implemented");
 }
 
 #endif
