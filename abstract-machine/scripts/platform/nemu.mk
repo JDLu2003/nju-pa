3726db927d5832b88c5d9a96177133a0b9dac8a3
JDLu2003 <1277319667@qq.com>
2026-02-10 11:59:06 +0800

    fix: 修复 ELF 文件参数传递问题
    - 在 nemu.mk 的 run 和 gdb 规则中直接指定 -e 参数
    - 确保 ELF 文件路径正确传递给 NEMU

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -30,9 +30,9 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS) -e $(IMAGE).elf" IMG=$(IMAGE).bin
 
 gdb: insert-arg
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS) -e $(IMAGE).elf" IMG=$(IMAGE).bin
 
 .PHONY: insert-arg
