75234cceac5a7ba2ff333db4266463bd638c5996
JDLu2003 <1277319667@qq.com.com>
2026-02-11 15:38:18 +0800

    fix: 修复 rtc 的读顺序问题

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -19,12 +19,19 @@
 
 static uint32_t *rtc_port_base = NULL;
 
+static uint64_t save_us;
+
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 4) {
-    uint64_t us = get_time();
-    rtc_port_base[0] = (uint32_t)us;
-    rtc_port_base[1] = us >> 32;
+  // if (!is_write && offset == 4) {
+  //   uint64_t us = get_time();
+  //   rtc_port_base[0] = (uint32_t)us;
+  //   rtc_port_base[1] = us >> 32;
+  // }
+  if (offset == 0) {
+    save_us = get_time();
+    rtc_port_base[0] = (uint32_t)save_us;
+    rtc_port_base[1] = (uint32_t)(save_us>>32);
   }
 }
 
